ac3845b38d372bbfd317b6073aeff2855eed777b
Sergei Sobol <sergei_sobol@tut.by>
2015-12-27 10:54:55 +0300

    Guess filename of source file

--- project/proglangs/tests.py
+++ project/proglangs/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
 
-# Create your tests here.
+from models import Compiler
+from utils import guess_filename
+
+
+class GuessFilenameTests(TestCase):
+    def test_java_1(self):
+        self.assertEqual(guess_filename(Compiler.JAVA), 'Solution.java')
+
+    def test_java_2(self):
+        code = 'public class solution {}'
+        self.assertEqual(guess_filename(Compiler.JAVA, code), 'solution.java')
+
+    def test_java_3(self):
+        code = '''
+        import java.io.File;
+        public class indic_sm extends Thread{
+        }
+        '''
+        self.assertEqual(guess_filename(Compiler.JAVA, code), 'indic_sm.java')
+
+    def test_java_4(self):
+        code = '''
+        // public class Old implements Runnable {
+        public class New implements Runnable {
+        '''
+        self.assertEqual(guess_filename(Compiler.JAVA, code), 'New.java')
--- project/proglangs/utils.py
+++ project/proglangs/utils.py
@@ -1,3 +1,5 @@
+import re
+
 from models import Compiler
 
 HIGHLIGHTJS = {
@@ -13,3 +15,46 @@ HIGHLIGHTJS = {
 
 def get_highlightjs_class(lang):
     return HIGHLIGHTJS.get(lang)
+
+
+FILENAME_EXTENSIONS = {
+    Compiler.C: 'c',
+    Compiler.CPP: 'cpp',
+    Compiler.JAVA: 'java',
+    Compiler.PASCAL: 'pas',
+    Compiler.DELPHI: 'dpr',
+    Compiler.PYTHON: 'py',
+    Compiler.CSHARP: 'cs',
+}
+
+PUBLIC_CLASS_REGEX = re.compile(r'^\s*public\s+class\s+([a-zA-Z0-9_]{1,64})')
+
+
+def _extract_java_public_class(text):
+    for line in text.splitlines():
+        m = PUBLIC_CLASS_REGEX.match(line)
+        if m is not None:
+            return m.group(1)
+
+
+def guess_filename(lang, text=None):
+    name = 'solution'
+
+    if lang == Compiler.JAVA:
+        name = 'Solution'
+        if text is not None:
+            public_class = _extract_java_public_class(text)
+            if public_class is not None:
+                name = public_class
+
+    elif lang == Compiler.CSHARP:
+        name = 'Solution'
+    else:
+        name = 'solution'
+
+    extension = FILENAME_EXTENSIONS.get(lang)
+
+    if extension is not None:
+        return '{0}.{1}'.format(name, extension)
+    else:
+        return name
