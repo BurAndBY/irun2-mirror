43b0b0e2c8a59bbf21f1c45492f389ea82b5525f
Sergei Sobol <sergei_sobol@tut.by>
2016-01-16 18:47:36 +0300

    Rename irunner_courses template tags

--- project/courses/templatetags/irunner_courses.py
+++ project/courses/templatetags/irunner_courses.py
@@ -4,8 +4,9 @@ register = template.Library()
 
 
 @register.inclusion_tag('courses/irunner_courses_criterion_tag.html')
-def irunner_courses_criterion(criterion, value):
+def irunner_courses_criterion(criterion, value, tooltip=False):
     return {
         'criterion': criterion,
         'value': value,
+        'tooltip': tooltip
     }
