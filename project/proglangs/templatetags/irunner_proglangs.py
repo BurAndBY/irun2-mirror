bb30b8ccbc256cd385f596e4804ee0d91d057d30
Sergei Sobol <sergei_sobol@tut.by>
2016-01-16 19:27:40 +0300

    One more move

--- project/proglangs/templatetags/irunner_proglangs.py
+++ project/proglangs/templatetags/irunner_proglangs.py
@@ -3,7 +3,7 @@ from django import template
 register = template.Library()
 
 
-@register.inclusion_tag('proglangs/irunner_proglangs_compilerbox.html')
+@register.inclusion_tag('proglangs/irunner_proglangs_compilerbox_tag.html')
 def irunner_proglangs_compilerbox(compiler, tooltip=False):
     return {
         'compiler': compiler,
