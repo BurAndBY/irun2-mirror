c2a0ad51ac0fcaa5b060d67adb8244105060abe6
Sergei Sobol <sergei_sobol@tut.by>
2016-02-26 22:03:24 +0300

    Less copypaste in urls

--- project/problems/urls.py
+++ project/problems/urls.py
@@ -1,27 +1,30 @@
-from django.conf.urls import url
+from django.conf.urls import include, url
 
 from . import views
+
+problem_urlpatterns = [
+    url(r'^$', views.ProblemOverviewView.as_view(), name='overview'),
+    url(r'^statement/(?P<filename>.+)?$', views.ProblemStatementView.as_view(), name='statement'),
+    url(r'^edit/$', views.ProblemEditView.as_view(), name='edit'),
+    url(r'^tests/$', views.ProblemTestsView.as_view(), name='tests'),
+    url(r'^tests/(?P<test_number>[0-9]+)/$', views.ProblemTestsTestView.as_view(), name='show_test'),
+    url(r'^tests/(?P<test_number>[0-9]+)/edit/$', views.ProblemTestsTestEditView.as_view(), name='edit_test'),
+    url(r'^solutions/$', views.ProblemSolutionsView.as_view(), name='solutions'),
+    url(r'^files/$', views.ProblemFilesView.as_view(), name='files'),
+    url(r'^files/data/(?P<file_id>[0-9]+)/get/(?P<filename>.*)$', views.ProblemFilesFileOpenView.as_view(), name='data_file_open'),
+    url(r'^files/data/(?P<file_id>[0-9]+)/edit/$', views.ProblemFilesDataFileEditView.as_view(), name='data_file_edit'),
+    url(r'^files/source/(?P<file_id>[0-9]+)/get/(?P<filename>.*)$', views.ProblemFilesSourceFileOpenView.as_view(), name='source_file_open'),
+    url(r'^files/source/(?P<file_id>[0-9]+)/edit/$', views.ProblemFilesSourceFileEditView.as_view(), name='source_file_edit'),
+    url(r'^submit/$', views.ProblemSubmitView.as_view(), name='submit'),
+    url(r'^submission/(?P<solution_id>[0-9]+)/$', views.ProblemSubmissionView.as_view(), name='submission'),
+]
 
 urlpatterns = [
     url(r'^tree/$', views.ShowTreeView.as_view(), name='show_tree'),
     url(r'^tree/(?P<folder_id>[0-9]+)/$', views.ShowTreeFolderView.as_view(), name='show_tree_folder'),
     url(r'^tree/(?P<folder_id>[0-9]+)/json/$', views.ShowTreeFolderJsonView.as_view(), name='show_tree_folder_json'),
 
-    url(r'^(?P<problem_id>[0-9]+)/$', views.ProblemOverviewView.as_view(), name='overview'),
-    url(r'^(?P<problem_id>[0-9]+)/statement/(?P<filename>.+)?$', views.ProblemStatementView.as_view(), name='statement'),
-    url(r'^(?P<problem_id>[0-9]+)/edit/$', views.ProblemEditView.as_view(), name='edit'),
-    url(r'^(?P<problem_id>[0-9]+)/tests/$', views.ProblemTestsView.as_view(), name='tests'),
-    #url(r'^(?P<problem_id>[0-9]+)/tests/add/$', views.add_test, name='add_test'),
-    url(r'^(?P<problem_id>[0-9]+)/tests/(?P<test_number>[0-9]+)/$', views.ProblemTestsTestView.as_view(), name='show_test'),
-    url(r'^(?P<problem_id>[0-9]+)/tests/(?P<test_number>[0-9]+)/edit/$', views.ProblemTestsTestEditView.as_view(), name='edit_test'),
-    url(r'^(?P<problem_id>[0-9]+)/solutions/$', views.ProblemSolutionsView.as_view(), name='solutions'),
-    url(r'^(?P<problem_id>[0-9]+)/files/$', views.ProblemFilesView.as_view(), name='files'),
-    url(r'^(?P<problem_id>[0-9]+)/files/data/(?P<file_id>[0-9]+)/get/(?P<filename>.*)$', views.ProblemFilesFileOpenView.as_view(), name='data_file_open'),
-    url(r'^(?P<problem_id>[0-9]+)/files/data/(?P<file_id>[0-9]+)/edit/$', views.ProblemFilesDataFileEditView.as_view(), name='data_file_edit'),
-    url(r'^(?P<problem_id>[0-9]+)/files/source/(?P<file_id>[0-9]+)/get/(?P<filename>.*)$', views.ProblemFilesSourceFileOpenView.as_view(), name='source_file_open'),
-    url(r'^(?P<problem_id>[0-9]+)/files/source/(?P<file_id>[0-9]+)/edit/$', views.ProblemFilesSourceFileEditView.as_view(), name='source_file_edit'),
-    url(r'^(?P<problem_id>[0-9]+)/submit/$', views.ProblemSubmitView.as_view(), name='submit'),
-    url(r'^(?P<problem_id>[0-9]+)/submission/(?P<solution_id>[0-9]+)/$', views.ProblemSubmissionView.as_view(), name='submission'),
+    url(r'^(?P<problem_id>[0-9]+)/', include(problem_urlpatterns)),
 
     url(r'^folders/(?P<folder_id_or_root>[0-9]+|root)/$', views.ShowFolderView.as_view(), name='show_folder'),
     url(r'^search/$', views.SearchView.as_view(), name='search'),
