8dd58dff67d60c03936a6ae140d3c38714376213
Sergei Sobol <sergei_sobol@tut.by>
2016-04-16 02:31:05 +0300

    Set plagiarism limit to 0.8

--- project/plagiarism/templatetags/irunner_plagiarism.py
+++ project/plagiarism/templatetags/irunner_plagiarism.py
@@ -17,7 +17,7 @@ def irunner_plagiarism_box(aggregated_result, solution_id):
     if aggregated_result:
         context['relevance'] = aggregated_result.relevance
         context['solution_id'] = solution_id
-        if aggregated_result.relevance > 0.5:
+        if aggregated_result.relevance > 0.8:
             context['style'] = 'danger'
         elif aggregated_result.relevance > 0.0:
             context['style'] = 'warning'
