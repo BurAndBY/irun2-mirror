ebb3a2f3cab98fc8e263f1b0f7b2f549d85dda89
Sergei Sobol <sergei_sobol@tut.by>
2016-04-15 18:20:53 +0300

    Improve visual representation of plagiarism score

---
+++ project/plagiarism/templatetags/irunner_plagiarism.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from django import template
+
+register = template.Library()
+
+
+@register.inclusion_tag('plagiarism/irunner_plagiarism_box_tag.html')
+def irunner_plagiarism_box(aggregated_result, solution_id):
+    '''
+    Displays plagiarism aggregated result.
+
+    args:
+        aggregated_result(plagiarism.models.JudgementResult)
+    '''
+    context = {}
+    if aggregated_result:
+        context['relevance'] = aggregated_result.relevance
+        context['solution_id'] = solution_id
+        if aggregated_result.relevance > 0.5:
+            context['style'] = 'danger'
+        elif aggregated_result.relevance > 0.0:
+            context['style'] = 'warning'
+
+    else:
+        context['relevance'] = None
+    return context
