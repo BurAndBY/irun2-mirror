1ea99d86698e5698596d104df06cadf26d3b0dfa
Sergei Sobol <sergei_sobol@tut.by>
2016-04-17 15:15:26 +0300

    Better list of contests

--- project/contests/globalviews.py
+++ project/contests/globalviews.py
@@ -1,5 +1,5 @@
 from django.db import transaction
-from django.shortcuts import redirect
+from django.shortcuts import render, redirect
 from django.views import generic
 
 from cauth.mixins import StaffMemberRequiredMixin
@@ -8,9 +8,20 @@ from proglangs.models import Compiler
 from .models import Contest
 
 
-class ContestListView(StaffMemberRequiredMixin, generic.ListView):
+class ContestListView(StaffMemberRequiredMixin, generic.View):
     model = Contest
     template_name = 'contests/contest_list.html'
+
+    def get(self, request):
+        year_to_contests = {}
+        for contest in Contest.objects.order_by('-start_time'):
+            year = contest.start_time.year
+            year_to_contests.setdefault(year, []).append(contest)
+
+        pairs = year_to_contests.items()
+        pairs.sort(reverse=True)
+
+        return render(request, self.template_name, {'pairs': pairs})
 
 
 class ContestCreateView(StaffMemberRequiredMixin, generic.CreateView):
