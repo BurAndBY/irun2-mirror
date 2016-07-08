0cfac4d0c9853d132d82788d992f3ea784065a13
Sergei Sobol <sergei_sobol@tut.by>
2016-07-08 20:39:23 +0300

    Move compiler command lines template to common code from contests

--- project/proglangs/templatetags/irunner_proglangs.py
+++ project/proglangs/templatetags/irunner_proglangs.py
@@ -1,5 +1,7 @@
 from django import template
 
+from proglangs.models import Compiler
+
 register = template.Library()
 
 
@@ -8,4 +10,16 @@ def irunner_proglangs_compilerbox(compiler, tooltip=False):
     return {
         'compiler': compiler,
         'tooltip': tooltip,
+    }
+
+
+@register.inclusion_tag('proglangs/irunner_proglangs_commandlines_tag.html')
+def irunner_proglangs_commandlines(compilers, expanded=False):
+    '''
+    args:
+        compilers: list of Compiler with CompilerDetails selected or a single Compiler
+    '''
+    return {
+        'compilers': [compilers] if isinstance(compilers, Compiler) else compilers,
+        'expanded': expanded
     }
--- project/proglangs/urls.py
+++ project/proglangs/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^create/$', views.CreateCompilerView.as_view(), name='create'),
+    url(r'^new/$', views.CreateCompilerView.as_view(), name='create'),
     url(r'^(?P<pk>[0-9]+)/$', views.UpdateCompilerView.as_view(), name='update'),
 ]
