11a847df8e4746bd554a7b55b43281250f74d24e
Sergei Sobol <sergei_sobol@tut.by>
2017-04-25 00:12:37 +0300

    Fix unittests

--- project/common/tests.py
+++ project/common/tests.py
@@ -122,19 +122,19 @@ class TimeTagsTests(TestCase):
     def test_timedelta_0(self):
         dt = datetime.timedelta(seconds=0)
         self.assertEqual(irunner_timedelta_hms(dt), '0:00:00')
-        self.assertEqual(irunner_timedelta_humanized(dt), '0\xa0seconds')
+        self.assertEqual(irunner_timedelta_humanized(dt), '0\xa0секунд')
 
     def test_timedelta_1(self):
         dt = datetime.timedelta(minutes=1, seconds=2)
         self.assertEqual(irunner_timedelta_hms(dt), '0:01:02')
-        self.assertEqual(irunner_timedelta_humanized(dt), '1\xa0минута, 2\xa0seconds')
+        self.assertEqual(irunner_timedelta_humanized(dt), '1\xa0минута 2\xa0секунды')
 
     def test_timedelta_2(self):
         dt = datetime.timedelta(hours=1, minutes=2, seconds=3)
         self.assertEqual(irunner_timedelta_hms(dt), '1:02:03')
-        self.assertEqual(irunner_timedelta_humanized(dt), '1\xa0час, 2\xa0минуты')
+        self.assertEqual(irunner_timedelta_humanized(dt), '1\xa0час 2\xa0минуты')
 
     def test_timedelta_3(self):
         dt = datetime.timedelta(days=7, hours=23, minutes=59, seconds=59)
         self.assertEqual(irunner_timedelta_hms(dt), '7 дней, 23:59:59')
-        self.assertEqual(irunner_timedelta_humanized(dt), '7\xa0дней, 23\xa0часа')
+        self.assertEqual(irunner_timedelta_humanized(dt), '7\xa0дней 23\xa0часа')
