ad07c05126d2699f7e426dfa4e58dd4f2e91b20a
Sergei Sobol <sergei_sobol@tut.by>
2017-04-24 21:55:12 +0300

    Better printing of time intervals

--- project/common/tests.py
+++ project/common/tests.py
@@ -1,7 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+import datetime
+
 from django.test import TestCase
 
 from common.enum import Enum
 from common.katex import parse_tex_math, Portion, PortionMode
+from common.templatetags.irunner_time import irunner_timedelta_hms, irunner_timedelta_humanized
 
 PM = PortionMode
 
@@ -107,3 +114,27 @@ class EnumTests(TestCase):
         for k, v in Color.iteritems():
             kvs.add((k, v))
         self.assertEqual(expected_kvs, kvs)
+
+
+class TimeTagsTests(TestCase):
+    # NOTE: \xa0 avoids wrapping between value and unit
+
+    def test_timedelta_0(self):
+        dt = datetime.timedelta(seconds=0)
+        self.assertEqual(irunner_timedelta_hms(dt), '0:00:00')
+        self.assertEqual(irunner_timedelta_humanized(dt), '0\xa0seconds')
+
+    def test_timedelta_1(self):
+        dt = datetime.timedelta(minutes=1, seconds=2)
+        self.assertEqual(irunner_timedelta_hms(dt), '0:01:02')
+        self.assertEqual(irunner_timedelta_humanized(dt), '1\xa0минута, 2\xa0seconds')
+
+    def test_timedelta_2(self):
+        dt = datetime.timedelta(hours=1, minutes=2, seconds=3)
+        self.assertEqual(irunner_timedelta_hms(dt), '1:02:03')
+        self.assertEqual(irunner_timedelta_humanized(dt), '1\xa0час, 2\xa0минуты')
+
+    def test_timedelta_3(self):
+        dt = datetime.timedelta(days=7, hours=23, minutes=59, seconds=59)
+        self.assertEqual(irunner_timedelta_hms(dt), '7 дней, 23:59:59')
+        self.assertEqual(irunner_timedelta_humanized(dt), '7\xa0дней, 23\xa0часа')
