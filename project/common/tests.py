cc6765cb393669b9c9635ccf24b24432053e64cc
Sergei Sobol <sergei_sobol@tut.by>
2020-02-19 03:24:47 +0300

    Remove enum implementation because it was not used

--- project/common/tests.py
+++ project/common/tests.py
@@ -6,79 +6,8 @@ import datetime
 
 from django.test import TestCase
 
-from common.enum import Enum
 from common.templatetags.irunner_time import irunner_timedelta_hms, irunner_timedelta_humanized
 from common.pylightex.tests import *  # noqa
-
-
-class Color(Enum):
-    RED = 1
-    GREEN = 2
-    BLUE = 3
-
-
-class ExtendedColor(Color):
-    BLACK = 0
-    COMPOSITE_NAME = -1
-
-
-class EnumTests(TestCase):
-    def test_basic(self):
-        self.assertEqual(Color.RED, 1)
-        self.assertEqual(Color.GREEN, 2)
-        self.assertEqual(Color.BLUE, Color['BLUE'])
-        self.assertEqual(Color.value2key(3), 'BLUE')
-
-        with self.assertRaises(AttributeError):
-            Color.BLACK
-
-    def test_get(self):
-        self.assertEqual(Color.get('GREEN'), 2)
-        self.assertEqual(Color.get('BLUE', 100500), 3)
-        self.assertIsNone(Color.get('UNKNOWN'))
-        self.assertEqual(Color.get('UNKNOWN', 42), 42)
-
-        with self.assertRaises(TypeError):
-            self.assertIsNone(Color.get(None))
-
-        with self.assertRaises(TypeError):
-            self.assertIsNone(Color.get(1))
-
-    def test_getitem(self):
-        self.assertEqual(Color['GREEN'], 2)
-
-        with self.assertRaises(KeyError):
-            Color['UNKNOWN']
-
-    def test_strings(self):
-        self.assertEqual(Color.value2key(1), 'RED')
-        self.assertEqual(Color.value2key(2), 'GREEN')
-
-        with self.assertRaises(ValueError):
-            Color.value2key(100500)
-
-    def test_inheritance(self):
-        self.assertEqual(ExtendedColor.RED, 1)
-        self.assertEqual(ExtendedColor.BLACK, 0)
-        self.assertEqual(ExtendedColor['COMPOSITE_NAME'], -1)
-        self.assertEqual(ExtendedColor.value2key(-1), 'COMPOSITE_NAME')
-
-    def test_iterate(self):
-        expected_ks = set(['RED', 'GREEN', 'BLUE'])
-        expected_kvs = set([('RED', 1), ('GREEN', 2), ('BLUE', 3)])
-
-        self.assertEqual(set(Color), expected_ks)
-        self.assertEqual(set(Color.iteritems()), expected_kvs)
-
-        ks = set()
-        for k in Color:
-            ks.add(k)
-        self.assertEqual(expected_ks, ks)
-
-        kvs = set()
-        for k, v in Color.iteritems():
-            kvs.add((k, v))
-        self.assertEqual(expected_kvs, kvs)
 
 
 class TimeTagsTests(TestCase):
