7f9fe86482798c6a62938e263616d25865be3776
Sergei Sobol <sergei_sobol@tut.by>
2020-02-22 19:20:05 +0300

    Activity page: improve perf and fix style

--- project/solutions/activity/views.py
+++ project/solutions/activity/views.py
@@ -22,22 +22,37 @@ class SingleDayInfo(object):
         self.accepted = 0
         self.rejected = 0
         self._max_value = None
-        self._height = None
-
-    def set_height(self, height):
-        self._height = float(height)
 
     def set_max_value(self, max_value):
         self._max_value = max_value if max_value > 0 else 1
 
-    def get_accepted_px(self):
-        return self._height * self.accepted / self._max_value
+    @staticmethod
+    def _perc(x):
+        return '{:.2f}%'.format(x)
 
-    def get_all_px(self):
-        return self._height * (self.accepted + self.rejected) / self._max_value
+    def get_accepted_perc(self):
+        return self._perc(100. * self.accepted / self._max_value)
+
+    def get_all_perc(self):
+        return self._perc(100. * (self.accepted + self.rejected) / self._max_value)
 
 
-def _make_chart(queryset, start_date, end_date, chart_height):
+def _make_solutions_qs(qs, start_date):
+    assert isinstance(start_date, datetime.date)
+    start_datetime = datetime.datetime.combine(start_date, datetime.time.min)
+    ts = timezone.make_aware(start_datetime)
+
+    first_id = qs.\
+        filter(reception_time__gte=ts).\
+        values_list('id', flat=True).\
+        first()
+
+    if first_id is not None:
+        return qs.filter(id__gte=first_id)
+    return qs.none()
+
+
+def _make_chart(queryset, start_date, end_date):
     day = datetime.timedelta(days=1)
     mp = {}
     results = []
@@ -48,10 +63,9 @@ def _make_chart(queryset, start_date, end_date, chart_height):
         mp[today] = info
         today += day
 
-    db_start_date = start_date - day
     tzinfo = timezone.get_current_timezone()
     for day, num_accepted, num_rejected in queryset.\
-            filter(reception_time__gte=db_start_date, best_judgement__status=Judgement.DONE).\
+            filter(best_judgement__status=Judgement.DONE).\
             annotate(
                 day=TruncDay('reception_time', tzinfo=tzinfo),
                 outcome=F('best_judgement__outcome')
@@ -73,11 +87,11 @@ def _make_chart(queryset, start_date, end_date, chart_height):
         max_value = max(max_value, info.accepted + info.rejected)
     for info in results:
         info.set_max_value(max_value)
-        info.set_height(chart_height)
     return results
 
 
 def _get_term_start(today):
+    assert isinstance(today, datetime.date)
     if today.month >= 9:
         term_start = datetime.date(today.year, 9, 1)
     elif today.month <= 1:
@@ -89,20 +103,19 @@ def _get_term_start(today):
 
 class ActivityView(generic.View):
     template_name = 'solutions/activity.html'
-    chart_height = 200
+    bar_max_px = 16
 
     def get(self, request):
         ts = timezone.now()
         today = timezone.localtime(ts).date()
 
-        results_year = _make_chart(Solution.objects.all(), today - datetime.timedelta(days=365), today, self.chart_height)
+        year_start = today - datetime.timedelta(days=365)
+        year_solution_queryset = _make_solutions_qs(Solution.objects, year_start)
+        results_year = _make_chart(year_solution_queryset, year_start, today)
 
         term_start = _get_term_start(today)
-        results_term = _make_chart(Solution.objects.filter(coursesolution__isnull=False), term_start, today, self.chart_height)
-
-        term_solution_queryset = Solution.objects.\
-            filter(coursesolution__isnull=False).\
-            filter(reception_time__gte=term_start)
+        term_solution_queryset = _make_solutions_qs(Solution.objects, term_start).filter(coursesolution__isnull=False)
+        results_term = _make_chart(term_solution_queryset, term_start, today)
 
         all_proglangbars = build_proglangbars(term_solution_queryset)
         accepted_proglangbars = build_proglangbars(term_solution_queryset.filter(best_judgement__status=Judgement.DONE, best_judgement__outcome=Outcome.ACCEPTED))
@@ -112,8 +125,8 @@ class ActivityView(generic.View):
         context = {
             'results_year': results_year,
             'results_term': results_term,
+            'max_width_px': self.bar_max_px * len(results_term),
             'term_start': term_start,
-            'chart_height': self.chart_height,
             'all_proglangbars': all_proglangbars,
             'accepted_proglangbars': accepted_proglangbars,
             'outcomebars': outcomebars,
