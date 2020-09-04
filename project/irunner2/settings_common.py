5f9c62513b3cfa1ded315cb930476889c3781e54
Sergei Sobol <sergei_sobol@tut.by>
2020-09-04 23:05:47 +0300

    Revert 5cd5cd2 to fix 500 error

--- project/irunner2/settings_common.py
+++ project/irunner2/settings_common.py
@@ -69,6 +69,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django_otp.middleware.OTPMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.locale.LocaleMiddleware',
