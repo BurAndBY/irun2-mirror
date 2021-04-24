21329ca278ae54458fafd12240a285ac8f5806b4
Alexander Kernozhitsky <sh200105@mail.ru>
2021-04-25 01:02:39 +0300

    Pass language info into plagiarism API

--- project/plagiarism/plagiarismstructs.py
+++ project/plagiarism/plagiarismstructs.py
@@ -1,6 +1,7 @@
 class PlagiarismSubJob(object):
-    def __init__(self, id, resource_id):
+    def __init__(self, id, language, resource_id):
         self.id = id
+        self.language = language
         self.resource_id = resource_id
 
 class PlagiarismTestingJob(object):
