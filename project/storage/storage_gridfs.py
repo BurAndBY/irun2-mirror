c8a664b51934a45561e48fd93d80bf8bed9cc298
Sergei Sobol <sergei_sobol@tut.by>
2023-07-22 22:19:32 +0300

    Remove debug output

--- project/storage/storage_gridfs.py
+++ project/storage/storage_gridfs.py
@@ -16,13 +16,11 @@ class GridFsStorage(DataStorage):
         self._fs = GridFSBucket(self._db)
 
     def _do_save(self, resource_id, f):
-        print('_do_save')
         with self._fs.open_upload_stream(str(resource_id)) as grid_in:
             for chunk in f.chunks():
                 grid_in.write(chunk)
 
     def _do_get_size_on_disk(self, resource_id):
-        print('_get_size_on_disk')
         try:
             with self._fs.open_download_stream_by_name(str(resource_id)) as grid_out:
                 return grid_out.length
@@ -30,7 +28,6 @@ class GridFsStorage(DataStorage):
             return 0
 
     def _do_serve(self, resource_id):
-        print('_do_serve')
         try:
             grid_out = self._fs.open_download_stream_by_name(str(resource_id))
             return ServedData(grid_out.length, FileWrapper(grid_out))
@@ -38,7 +35,6 @@ class GridFsStorage(DataStorage):
             return None
 
     def _do_read_with_size(self, resource_id, max_size):
-        print('_do_read_with_size')
         try:
             with self._fs.open_download_stream_by_name(str(resource_id)) as grid_out:
                 blob = grid_out.read() if max_size is None else grid_out.read(max_size)
@@ -47,7 +43,6 @@ class GridFsStorage(DataStorage):
             return (None, 0)
 
     def _do_get_existing_files(self, resource_ids):
-        print('_do_get_existing_files')
         names = [str(resource_id) for resource_id in resource_ids]
         res = set()
         for fd in self._fs.find({"filename": {"$in": names}}):
